d91d6825343b2297894f3a0544c46573ca0a4fc2
Ankit Kumar <ankitkumardos47@gmail.com>
2022-12-31 11:50:32 +0530

    changed package.json

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const bodyParser= require('body-parser');
-require('./db/conn');
+require('./src/db/conn');
 const cors=require('cors');
 const app=express();
 const dotenv=require('dotenv');
@@ -11,8 +11,8 @@ const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json({limit:'30mb',extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
-app.use(require('./routers/router'));
-app.use(require('./routers/auth'));
+app.use(require('./src/routers/router'));
+app.use(require('./src/routers/auth'));
 
 
 app.listen(port,()=>{
